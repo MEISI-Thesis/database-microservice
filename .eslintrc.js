685f585ffe482a8cad455967899d6ccc8275a45f
Diogo <diogorafaellourencooliveira97@gmail.com>
2023-03-12 15:14:25 +0000

    Worked on Prisma

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+    jest: true
+  },
+  extends: ['standard-with-typescript'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname
+  },
+  rules: {
+    '@typescript-eslint/array-type': 'off',
+    '@typescript-eslint/semi': 'off',
+    semi: 'off',
+    'space-before-function-paren': 'off',
+    '@typescript-eslint/space-before-function-paren': 'off'
+  }
+}
---
+++ src/services/database.service.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from '@prisma/client';
+
+export class DatabaseService {
+  private readonly _client: PrismaClient = new PrismaClient();
+
+  public get client(): PrismaClient {
+    return this._client;
+  }
+}
