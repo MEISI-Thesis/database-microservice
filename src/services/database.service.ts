5c484a0c8ddba0b4aba2685dfcd0bab59bb8f281
Diogo <diogorafaellourencooliveira97@gmail.com>
2023-03-14 11:25:52 +0000

    Rebase

--- src/services/database.service.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-export class DatabaseService {
-  private readonly _client: PrismaClient = new PrismaClient();
-
-  public get client(): PrismaClient {
-    return this._client;
-  }
-}
